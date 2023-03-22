23e3a7bf5e0edd6fc93616b75decbb6d4f0c1a43
@Tuan2003 <nvt2602>
2023-03-22 15:09:26 +0700

    Tuan

---
+++ flutter_hive_tdo/lib/utils/strings.dart
@@ -0,0 +1,17 @@
+class MyString {
+  static const String mainTitle = "Công việc của tôi";
+  static const String deletedTask = "Nhiệm vụ này đã bị xóa";
+  static const String doneAllTask = "Bạn đã hoàn thành mọi nhiệm vụ ";
+  static const String addNewTask = "Thêm công việc mới ";
+  static const String updateCurrentTask = "Cập nhập ";
+  static const String taskStrnig = "Task";
+  static const String titleOfTitleTextField = "Bạn đang lên kế hoạch gì";
+  static const String addNote = 'Nội dung ghi chú';
+  static const String timeString = "Thời gian";
+  static const String dateString = "Ngày";
+  static const String deleteTask = "Xóa nhiệm vụ";
+  static const String addTaskString = "Thêm nhiệm vụ";
+  static const String updateTaskString = "Cập nhập thêm nhiệm vụ";
+  static const String oopsMsg = "Lỗi !";
+  static const String areYouSure = "Bạn có chắc chắn không ?";
+}
